038f8bf072473a4402933fb80d31b08c4462823f
Mike <killeymic@gmail.com>
2026-01-17 10:37:36 -0800

    Renamed mod to Station Troops With Fleet.

---
+++ src/StationTroopsWithFleet/SaveableTypeDefiner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using StationTroopsWithFleet.Station;
+
+namespace StationTroopsWithFleet
+{
+    internal class SaveableTypeDefiner : TaleWorlds.SaveSystem.SaveableTypeDefiner
+    {
+        public SaveableTypeDefiner() : base((0x1f3dd5 << 8) | 123) { }
+
+        protected override void DefineClassTypes()
+        {
+            AddClassDefinition(typeof(StationedTroops), 1);
+            AddClassDefinition(typeof(StationedTroopsManager), 2);
+        }
+
+        protected override void DefineContainerDefinitions()
+        {
+            ConstructContainerDefinition(typeof(List<StationedTroops>));
+        }
+    }
+}
---
+++ src/StationTroopsWithFleet/Station/StationedTroops.cs
@@ -0,0 +1,26 @@
+﻿using TaleWorlds.CampaignSystem.Naval;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Roster;
+using TaleWorlds.SaveSystem;
+
+namespace StationTroopsWithFleet.Station
+{
+    internal class StationedTroops
+    {
+        [SaveableProperty(1)]
+        public AnchorPoint AnchorPoint { get; private set; }
+
+        [SaveableProperty(2)]
+        public TroopRoster MemberRoster { get; set; }
+
+        [SaveableProperty(3)]
+        public TroopRoster PrisonRoster { get; set; }
+
+        public StationedTroops(AnchorPoint anchorPoint, TroopRoster memberRoster, TroopRoster prisonRoster)
+        {
+            AnchorPoint = anchorPoint;
+            MemberRoster = memberRoster;
+            PrisonRoster = prisonRoster;
+        }
+    }
+}
